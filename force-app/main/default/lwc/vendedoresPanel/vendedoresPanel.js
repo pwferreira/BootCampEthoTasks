a4a9c4d5632e73817ef736cae30dce70ee474718
Patrick Ferreira <pwferreira.info@gmail.com>
2023-03-09 17:17:53 -0300

    Commit painel de vendedor funcionando normalmente

--- force-app/main/default/lwc/vendedoresPanel/vendedoresPanel.js
+++ force-app/main/default/lwc/vendedoresPanel/vendedoresPanel.js
@@ -1,17 +1,16 @@
-import { LightningElement,wire,track } from 'lwc';
+import { LightningElement,wire,track,api } from 'lwc';
 import getAccountList from "@salesforce/apex/VendedoresPanelController.getAllAccountsByVendedor";
 
 
 export default class VendedoresPanel extends LightningElement {
    
-  center = {location: { 
-    Latitude: '-22.4379162',
-    Longitude: '-46.9619559',
-  }};
+  center;
 
   zoomLevel = 10;
   mapMarkers = [];
   error;
+  showMap = false;
+  @api disableChildRows; 
 
   @track gridColumns = [
     {label : 'Nome' , fieldName : 'Name', type: 'text'},
@@ -22,61 +21,105 @@ export default class VendedoresPanel extends LightningElement {
   @track gridData;
 
   connectedCallback(){
-      getAccountList()
-      .then(result =>{
-        const temp = JSON.parse(JSON.stringify(result));
+      this.initializeVendedorData();
+      this.initializeMapData();
+  }
 
-        console.log('temp....' + JSON.stringify(temp));
-        for(var i=0; i<temp.length ; i++){
-          var relatedAccount = temp[i]['Accounts__r'];
+  initializeVendedorData() {
+    getAccountList()
+      .then(result =>{
+        const vendedoresList = JSON.parse(JSON.stringify(result));
+
+        console.log('vendedoresList....' + JSON.stringify(vendedoresList));
+        for(var i=0; i<vendedoresList.length ; i++){
+          var relatedAccount = vendedoresList[i]['Accounts__r'];
           console.log('**Account....' + JSON.stringify(relatedAccount));
 
-          var salesPersonType = temp[i]['RecordType']['DeveloperName'];
+          var salesPersonType = vendedoresList[i]['RecordType']['DeveloperName'];
           console.log('SalesPerson...:' + salesPersonType);
 
-          temp[i]['SalesPersonType'] = salesPersonType;
+          vendedoresList[i]['SalesPersonType'] = salesPersonType;
 
-          temp[i]['Qty'] = relatedAccount.length ;
+          vendedoresList[i]['Qty'] = relatedAccount.length ;
           console.log('Qtd Contas: ' + relatedAccount.length);
 
           if(relatedAccount){
-            temp[i]._children = relatedAccount;
-            delete temp[i].Accounts__r;
+            this.disableChildRows = true;
+            vendedoresList[i]._children = relatedAccount;
           }
         }
-       
-        this.gridData = temp; 
-
-        console.log('temp final data....' + JSON.stringify(this.gridData));
-
+        
+        this.gridData = vendedoresList; 
+        
       })
       .catch(error =>{
         console.error(error);
-      })
+      });
+  }  
+
+  initializeMapData() {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.center = {
+          location: {
+              Latitude: position.coords.latitude,
+              Longitude: position.coords.longitude
+          }
+      };
+  });
   }
 
   handleRowSelection(event) {
     const selectedRows = event.detail.selectedRows;
-    if (selectedRows.length === 1) {
-        const selectedRow = selectedRows[0];
-        console.log('selected row....' + JSON.stringify(selectedRow));
 
-        for(var i=0; i<temp.length ; i++){
-          var relatedAccount = temp[i]['Accounts__r'];
-          if(relatedAccount){
-           
-          }
-          else if (error) {
-            this.error = error;
-            this.mapMarkers = undefined;
-          }
+    console.log('selectedRows.length....' + selectedRows.length);
+    console.log('selectedRows object....' + JSON.stringify(selectedRows));
 
-        }
+    if (selectedRows.length === 1){
+      let mapMarkers =  [];
+      let zoomLevel = 10;
 
-    }
+      if(selectedRows[0]['level'] != 1){
+        console.log('selectedRows[0]....' + JSON.stringify(selectedRows[0]));
+        
+          mapMarkers.push({
+            location: {
+              Street: selectedRows[0]['EnderecoRua__c'],
+              City: selectedRows[0]['EnderecoCidade__c'],
+              State: selectedRows[0]['EnderecoEstado__c']
+            },
+  
+            title: selectedRows[0]['Name'],
+            icon: 'standard:account'
+          });
+
+          zoomLevel = 15;
+
+      }
+      else{
+        var relatedAccounts = selectedRows[0]['Accounts__r'];  
+
+        relatedAccounts.forEach(account => {
+          mapMarkers.push({
+              location: {
+                Street: account.EnderecoRua__c,
+                City: account.EnderecoCidade__c,
+                State: account.EnderecoEstado__c
+              },
+    
+              title: account.Name,
+              icon: 'standard:account'
+            });
+        });
+      }
+      this.mapMarkers = mapMarkers;
+      this.showMap = true;
+      this.zoomLevel = zoomLevel;
+    }        
+  }
+    
 }
 
-}
+
 
 
 
